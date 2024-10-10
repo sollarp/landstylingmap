78af22853ff31ae562d23c071229ee5a64d372a7
Peter Szollar <peter@Peters-MacBook-Pro.local>
2024-10-10 22:56:42 +0200

    add multiPolygonData

--- app/src/main/java/com/ldstack/stylinggooglemap/data/dto/DataX.kt
+++ app/src/main/java/com/ldstack/stylinggooglemap/data/dto/DataX.kt
@@ -17,3 +17,26 @@ fun List<Site2>.toDataModel(): List<DataModel> {
     }
 }
 
+object GeometryConverter {
+    fun parseData(jsonString: String): List<List<Double>>  {
+        val jsonElement = Json.parseToJsonElement(jsonString)
+        val jsonObject = jsonElement.jsonObject
+
+        val type = jsonObject["type"]?.jsonPrimitive?.content
+
+        return when (type) {
+            "Polygon" -> {
+                // Assuming PolygonType is your data class for Polygon type
+                val polygonData = Json.decodeFromString<PolygonType>(jsonString) // Handle Polygon case
+                polygonData.coordinates[0] // Return the first set of coordinates
+            }
+            "MultiPolygon" -> {
+                val multiPolygonData = Json.decodeFromString<MultiPolygonType>(jsonString) // Handle MultiPolygon case
+                println("multiPolygonData: ${multiPolygonData.coordinates}")
+                multiPolygonData.coordinates[0].map { it[0] } // Return the first set of coordinates
+            }
+             // Return null or handle other cases
+            else -> { emptyList() }
+        }
+    }
+}
